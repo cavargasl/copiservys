ca7d2d4468ef9807b417ec0d9fa13710a4ff12fc
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-21 16:30:11 -0500

    feat: implement service y the view

--- app/(home)/products/page.tsx
+++ app/(home)/products/page.tsx
@@ -1,10 +1,15 @@
-"use client";
 import { ProductCard } from "@/components/ProductCard";
 import { Separator } from "@/components/ui/separator";
-import { sampleProducts } from "../components/RecommendedProducts";
 import Filter from "./components/Filter";
+import { productService } from "@/core/products/application/productService";
+import { excelProductsRepository } from "@/core/products/infrastructure/excelProducts.repository";
+import { env } from "@/config/env.mjs";
 
-export default function ProductsPage() {
+export const revalidate = 86400;
+
+export default async function ProductsPage() {
+  const products = await productService(excelProductsRepository(env.PRODUCTS_SHEET_URL)).getProducts()
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-[280px_auto_1fr] gap-4">
@@ -12,7 +17,7 @@ export default function ProductsPage() {
         <Separator orientation="vertical" className="h-auto" />
 
         <div className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))]">
-          {sampleProducts.map((product) => (
+          {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
