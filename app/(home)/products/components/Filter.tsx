2015a363b925965eed924c6af1d8a084cda80cec
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-21 17:00:51 -0500

    feat: get filter from petition

--- app/(home)/products/components/Filter.tsx
+++ app/(home)/products/components/Filter.tsx
@@ -1,32 +1,35 @@
 "use client";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Slider } from "@/components/ui/slider";
 import { formatPrice } from "@/core/shared/utils";
 import { Search, X } from "lucide-react";
 import { useState } from "react";
 
-const categories = [
-  { id: 1, name: "Smartphones", count: 24 },
-  { id: 2, name: "Laptops", count: 18 },
-  { id: 3, name: "Tablets", count: 12 },
-  { id: 4, name: "Auriculares", count: 30 },
-  { id: 5, name: "Smartwatches", count: 15 },
-  { id: 6, name: "Accesorios", count: 45 },
-] as const;
+interface FilterItem {
+  name: string;
+  count: number;
+}
+interface PriceRange {
+  min: number;
+  max: number;
+}
 
-const brands = [
-  { id: 1, name: "Samsung", count: 42 },
-  { id: 2, name: "Apple", count: 38 },
-  { id: 3, name: "Xiaomi", count: 25 },
-  { id: 4, name: "Huawei", count: 20 },
-  { id: 5, name: "Sony", count: 15 },
-  { id: 6, name: "LG", count: 12 },
-] as const;
+interface FilterProps {
+  categories: FilterItem[];
+  brands: FilterItem[];
+  priceRange: PriceRange;
+}
 
-const maxPrice = 5000000;
-
-export default function Filter() {
-  const [priceRange, setPriceRange] = useState<number[]>([0, maxPrice]);
+export default function Filter({
+  categories,
+  brands,
+  priceRange,
+}: FilterProps) {
+  const [selectedPriceRange, setSelectedPriceRange] = useState<number[]>([
+    0,
+    Math.ceil(priceRange.max),
+  ]);
   const [searchText, setSearchText] = useState("");
 
   return (
@@ -35,11 +38,11 @@ export default function Filter() {
       <div>
         <div className="relative">
           <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input 
+          <Input
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Buscar productos..." 
-            className="pl-8 bg-secondary/50 hover:bg-secondary/70 focus:bg-secondary/70 transition-colors" 
+            placeholder="Buscar productos..."
+            className="pl-8 bg-secondary/50 hover:bg-secondary/70 focus:bg-secondary/70 transition-colors"
           />
           {searchText && (
             <button
@@ -58,12 +61,12 @@ export default function Filter() {
         <div className="space-y-2">
           {categories.map((category) => (
             <label
-              key={category.id}
+              key={category.name}
               className="flex items-center justify-between"
             >
               <div className="flex items-center">
-                <input type="checkbox" className="form-checkbox" />
-                <span className="ml-2">{category.name}</span>
+                <Checkbox />
+                <span className="ml-2 capitalize">{category.name}</span>
               </div>
               <span className="text-sm text-muted-foreground">
                 ({category.count})
@@ -78,10 +81,13 @@ export default function Filter() {
         <h3 className="font-medium mb-2">Marcas</h3>
         <div className="space-y-2">
           {brands.map((brand) => (
-            <label key={brand.id} className="flex items-center justify-between">
+            <label
+              key={brand.name}
+              className="flex items-center justify-between"
+            >
               <div className="flex items-center">
-                <input type="checkbox" className="form-checkbox" />
-                <span className="ml-2">{brand.name}</span>
+                <Checkbox />
+                <span className="ml-2 capitalize">{brand.name}</span>
               </div>
               <span className="text-sm text-muted-foreground">
                 ({brand.count})
@@ -95,17 +101,16 @@ export default function Filter() {
       <div className="space-y-2">
         <span className="font-medium">Rango de precio:</span>
         <Slider
-          defaultValue={priceRange}
-          max={maxPrice}
-          step={1000}
-          minStepsBetweenThumbs={10}
-          value={priceRange}
-          onValueChange={setPriceRange}
+          defaultValue={selectedPriceRange}
+          max={Math.ceil(priceRange.max)}
+          min={0}
+          value={selectedPriceRange}
+          onValueChange={setSelectedPriceRange}
           isRange
         />
         <div className="flex justify-between text-sm mt-2">
-          <span>{formatPrice(priceRange[0])}</span>
-          <span>{formatPrice(priceRange[1])}</span>
+          <span>{formatPrice(selectedPriceRange[0])}</span>
+          <span>{formatPrice(selectedPriceRange[1])}</span>
         </div>
       </div>
     </div>
--- app/(home)/products/page.tsx
+++ app/(home)/products/page.tsx
@@ -2,18 +2,50 @@ import { ProductCard } from "@/components/ProductCard";
 import { Separator } from "@/components/ui/separator";
 import Filter from "./components/Filter";
 import { productService } from "@/core/products/application/productService";
-import { excelProductsRepository } from "@/core/products/infrastructure/excelProducts.repository";
-import { env } from "@/config/env.mjs";
+import { testProductsRepository } from "@/core/products/infrastructure/testProducts.repository";
 
 export const revalidate = 86400;
 
 export default async function ProductsPage() {
-  const products = await productService(excelProductsRepository(env.PRODUCTS_SHEET_URL)).getProducts()
+  //const products = await productService(excelProductsRepository(env.PRODUCTS_SHEET_URL)).getProducts()
+  const products = await productService(testProductsRepository()).getProducts()
+  
+  const categoriesWithCount = products.reduce((acc, product) => {
+    acc[product.category] = (acc[product.category] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const brandsWithCount = products.reduce((acc, product) => {
+    acc[product.brand] = (acc[product.brand] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const priceRange = products.reduce(
+    (acc, product) => ({
+      min: Math.min(acc.min, product.price),
+      max: Math.max(acc.max, product.price)
+    }),
+    { min: Infinity, max: -Infinity }
+  );
+
+  const categories = Object.entries(categoriesWithCount).map(([name, count]) => ({
+    name,
+    count
+  }));
+
+  const brands = Object.entries(brandsWithCount).map(([name, count]) => ({
+    name,
+    count
+  }));
 
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-[280px_auto_1fr] gap-4">
-        <Filter />
+        <Filter 
+          categories={categories} 
+          brands={brands}
+          priceRange={priceRange}
+        />
         <Separator orientation="vertical" className="h-auto" />
 
         <div className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))]">
