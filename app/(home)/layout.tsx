1203385a94ffaf1f424c01c7b30ca02fc8ea3baf
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-22 13:02:51 -0500

    fix: error to build, need useSearchParams need wrapper Suspense

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,9 +1,11 @@
-import SiteHeader from "@/components/layouts/SiteHeader"
+import SiteHeader from "@/components/layouts/SiteHeader";
 import { fetchProducts } from "@/redux/slices/products";
 import { store } from "@/redux/store";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 interface LobbyLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default async function LobbyLayout({ children }: LobbyLayoutProps) {
@@ -12,8 +14,16 @@ export default async function LobbyLayout({ children }: LobbyLayoutProps) {
     <div className="relative flex min-h-screen flex-col">
       <SiteHeader />
       <main className="flex-1">
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center min-h-screen">
+              <Loader2 className="h-8 w-8 animate-spin" />
+            </div>
+          }
+        >
           {children}
+        </Suspense>
       </main>
     </div>
-  )
+  );
 }
