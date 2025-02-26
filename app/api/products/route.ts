3ed3ae7138f45de1063e21b7b62c93550436a581
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-26 11:55:01 -0500

    feat: add more data example for technical test

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,12 +1,13 @@
-import { env } from "@/config/env.mjs";
+//import { env } from "@/config/env.mjs";
 import { productService } from "@/core/products/application/productService";
-import { excelProductsRepository } from "@/core/products/infrastructure/excelProducts.repository";
-//import { JsonProductsRepository } from "@/core/products/infrastructure/jsonProducts.repository";
+//import { excelProductsRepository } from "@/core/products/infrastructure/excelProducts.repository";
+import { JsonProductsRepository } from "@/core/products/infrastructure/jsonProducts.repository";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const products = await productService(excelProductsRepository(env.NEXT_PUBLIC_PRODUCTS_SHEET_URL)).getProducts();
+    const products = await productService(JsonProductsRepository()).getProducts();
+    //const products = await productService(excelProductsRepository(env.NEXT_PUBLIC_PRODUCTS_SHEET_URL)).getProducts();
     return NextResponse.json(products, { status: 200 });
   } catch (error) {
     console.error("Error obteniendo productos del json", error);
