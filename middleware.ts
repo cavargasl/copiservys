115f58b1accc14a70949762cf947aa007d147da1
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-28 12:19:43 -0500

    update: enabled CORS in middleware

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const allowedOrigins = ['https://copiservys.com', 'https://www.copiservys.com', 'https://copiservys.vercel.app'];
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
+export function middleware(req: NextRequest) {
+  const origin = req.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  const response = NextResponse.next()
+ 
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+ 
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value)
+  })
+ 
+  return response
+}
+
+
+export const config = {
+  matcher: ['/api/:path*'],
+};
