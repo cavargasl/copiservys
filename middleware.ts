37b5dbc3fa254231a0f7664a32a54998e99621bd
Camilo Vargas <camilovargasingm@gmail.com>
2025-02-28 12:42:58 -0500

    testing

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,9 @@ const corsOptions = {
 
 export function middleware(req: NextRequest) {
   const origin = req.headers.get('origin') ?? ''
+  console.log(origin, 'origin')
   const isAllowedOrigin = allowedOrigins.includes(origin)
-
+  console.log(isAllowedOrigin, 'isAllowedOrigin')
   const response = NextResponse.next()
  
   if (isAllowedOrigin) {
@@ -19,6 +20,7 @@ export function middleware(req: NextRequest) {
   Object.entries(corsOptions).forEach(([key, value]) => {
     response.headers.set(key, value)
   })
+  console.log(response.headers.get('Access-Control-Allow-Origin'), 'response.headers.get')
  
   return response
 }
