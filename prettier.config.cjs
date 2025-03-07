1bcb3247bd0bbb9b9828f3e8112b995c03c454ca
Camilo Vargas <camilovargasingm@gmail.com>
2025-03-07 15:13:57 -0500

    add prettier setting file

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  quoteProps: 'preserve',
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  printWidth: 100,
+}
